92264334390dd8ce7a01126e9aedda96ee87a3d4
Ricardo Do Vale <pataco80@gmail.com>
2023-11-25 12:51:11 +0100

    test ajout manuel

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,15 +16,13 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-image`,
+    `gatsby-plugin-static-cms`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
-    },
-    {
-      resolve: `gatsby-plugin-static-cms`,
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
